2778c953e8c6fdfb2d3dc4f94d06ba4343f6bc6b
Sid Palas <sid.palas@gmail.com>
2018-06-01 10:32:01 +0200

    fixed line endings in ESTK

--- main.jsx
+++ main.jsx
@@ -1,1 +1,12 @@
-﻿#target photoshop//@include "./logging.jsxlib"var defaultLog = new LogFile();defaultLog.log("Logging to the default log!");defaultLog.log("Logging to the default log again...");var normalLog = new LogFile('normal');normalLog.log("Logging to the normal log!");var specialLog = new LogFile('special');specialLog.log("Logging to the special log!");
+﻿#target photoshop
+//@include "./logging.jsxlib"
+
+var defaultLog = new LogFile();
+defaultLog.log("Logging to the default log!");
+defaultLog.log("Logging to the default log again...");
+
+var normalLog = new LogFile('normal');
+normalLog.log("Logging to the normal log!");
+
+var specialLog = new LogFile('special');
+specialLog.log("Logging to the special log!");
