d61a91a059737e02bc28b3c09d54481ad291be04
MaxJohnson <MaxJohnson@users.noreply.github.com>
2018-12-13 13:49:43 -0800

    Add files via upload
    - adding cross compatibility with ES5/6 Shims for Date.getFullYear() vs getYear() so there's no runtime error using shims", exposing type and directory path as properties, adding function and example for .removeOld() to clear out old logs.

--- main.jsx
+++ main.jsx
@@ -10,3 +10,15 @@ normalLog.log("Logging to the normal log!");
 
 var specialLog = new LogFile('special');
 specialLog.log("Logging to the special log!");
+
+var removeMeLog = new LogFile('removeMe');
+removeMeLog.log("Logging to the removeMe log!");
+
+removeMeLog = new LogFile('removeMe');
+removeMeLog.log("Logging to the *new* removeMe log!");
+
+removeMeLog = new LogFile('removeMe');
+removeMeLog.log("Logging to the *new**new* removeMe log!");
+
+removeMeLog.removeOld();// or .removeOld( typeString, dateObject)
+removeMeLog.log("Logging to the *new**new**new* removeMe log!");
